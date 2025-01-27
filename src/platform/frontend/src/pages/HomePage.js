63e4ac1fb9370fa52ffbd77c0397789c73b368a9
Fast777 <194656846+Fast677@users.noreply.github.com>
2025-01-26 20:11:34 -0800

    Update HomePage.js

--- src/platform/frontend/src/pages/HomePage.js
+++ src/platform/frontend/src/pages/HomePage.js
@@ -1,1 +1,12 @@
+// src/platform/frontend/src/pages/HomePage.js
+import React from 'react';
 
+const HomePage = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a Alphapp!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
