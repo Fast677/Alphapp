0caf0cd8e7363bae46bf858a9e9e5934a44ec272
Fast777 <194656846+Fast677@users.noreply.github.com>
2025-02-10 19:37:25 -0800

    Update ExampleUnitTest.java

--- src/platform/mobile/android/app/src/main/tests/ExampleUnitTest.java
+++ src/platform/mobile/android/app/src/main/tests/ExampleUnitTest.java
@@ -1,1 +1,26 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(0, 2 - 2);
+    }
+
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals(9, 3 * 3);
+    }
+
+    @Test
+    public void division_isCorrect() {
+        assertEquals(2, 4 / 2);
+    }
+}
