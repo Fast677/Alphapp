02764995d392d36a4ba0712b59b75eee0133bc29
Fast777 <194656846+Fast677@users.noreply.github.com>
2025-02-20 09:22:30 -0800

    Update __init__.py

--- src/community/backend/models/__init__.py
+++ src/community/backend/models/__init__.py
@@ -1,1 +1,7 @@
+# src/community/backend/models/__init__.py
 
+from .user import User
+from .resource import Resource
+# Aquí se pueden añadir más importaciones de otros modelos
+
+__all__ = ['User', 'Resource'] # Opcional: Define qué nombres se exportan al usar from models import *
